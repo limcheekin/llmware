85fa5f4b054c4994a00caf033103452fa4d2ab28
limcheekin <limcheekin@vobject.com>
2024-10-16 17:30:34 +0800

    feat: update using-ollama-models to use the smallest model `qwen2.5:0.5b` and update gitignore

--- examples/Models/using-ollama-models.py
+++ examples/Models/using-ollama-models.py
@@ -9,45 +9,27 @@ from llmware.models import ModelCatalog
 #   -- these two lines will register: llama2 and mistral models
 #   -- note: assumes that you have previously cached and installed both of these models with ollama locally
 
-#   register llama2
-ModelCatalog().register_ollama_model(model_name="llama2",model_type="chat",host="localhost",port=11434)
+model_name = "qwen2.5:0.5b"
 
-#   register mistral - note: if you are using ollama defaults, then OK to register with ollama model name only
-ModelCatalog().register_ollama_model(model_name="mistral")
+#   register model
+ModelCatalog().register_ollama_model(model_name=model_name,model_type="chat",host="localhost",port=11434)
 
 #   optional - confirm that model was registered
-my_new_model_card = ModelCatalog().lookup_model_card("llama2")
-print("\nupdate: confirming - new ollama model card - ", my_new_model_card)
+my_new_model_card = ModelCatalog().lookup_model_card(model_name)
+print("\nupdate: confirming - new ollama " + model_name + " model card - ", my_new_model_card)
 
 #   Step 2 - start using the Ollama model like any other model in llmware
 
-print("\nupdate: calling ollama llama 2 model ...")
+print("\nupdate: calling ollama model ...")
 
-model = ModelCatalog().load_model("llama2")
+model = ModelCatalog().load_model(model_name)
 response = model.inference("why is the sky blue?")
 
-print("update: example #1 - ollama llama 2 response - ", response)
-
-#   Tip: if you are loading 'llama2' chat model from Ollama, note that it is already included in
-#   the llmware model catalog under a different name, "TheBloke/Llama-2-7B-Chat-GGUF"
-#   the llmware model name maps to the original HuggingFace repository, and is a nod to "TheBloke" who has
-#   led the popularization of GGUF - and is responsible for creating most of the GGUF model versions.
-#   --llmware uses the "Q4_K_M" model by default, while Ollama generally prefers "Q4_0"
-
-print("\nupdate: calling Llama-2-7B-Chat-GGUF in llmware catalog ...")
-
-model = ModelCatalog().load_model("TheBloke/Llama-2-7B-Chat-GGUF")
-response = model.inference("why is the sky blue?")
-
-print("update: example #1 - [compare] - llmware / Llama-2-7B-Chat-GGUF response - ", response)
-
-#   Now, let's try the Ollama Mistral model with a context passage
-
-model2 = ModelCatalog().load_model("mistral")
+print("update: example #1 - ollama " + model_name + " response - ", response)
 
 context_passage= ("NASA’s rover Perseverance has gathered data confirming the existence of ancient lake "
                   "sediments deposited by water that once filled a giant basin on Mars called Jerezo Crater, "
-                  "according to a study published on Friday.  The findings from ground-penetrating radar "
+                  "according to a study published on Friday. The findings from ground-penetrating radar "
                   "observations conducted by the robotic rover substantiate previous orbital imagery and "
                   "other data leading scientists to theorize that portions of Mars were once covered in water "
                   "and may have harbored microbial life.  The research, led by teams from the University of "
@@ -57,15 +39,15 @@ context_passage= ("NASA’s rover Perseverance has gathered data confirming the 
                   "crater floor onto an adjacent expanse of braided, sedimentary-like features resembling, "
                   "from orbit, the river deltas found on Earth.")
 
-response = model2.inference("What are the top 3 points?", add_context=context_passage)
+response = model.inference("What are the top 3 points?", add_context=context_passage)
 
-print("\nupdate: calling ollama mistral model ...")
+print("\nupdate: calling ollama " + model_name + " model ...")
 
-print("update: example #2 - ollama mistral response - ", response)
+print("update: example #2 - ollama " + model_name + " response - ", response)
 
 #   Step 3 - using the ollama discovery API - optional
 
-discovery = model2.discover_models()
+discovery = model.discover_models()
 print("\nupdate: example #3 - checking ollama model manifest list: ", discovery)
 
 if len(discovery) > 0:
